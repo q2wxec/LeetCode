48dfb27d3d9dae163885eaf9d2c02b4b5a4ae837
wangxiao18 <wangxiao18@xiaomi.com>
2021-05-19 10:00:11 +0800

    队列问题

--- src/com/algorithm/QueueSet.java
+++ src/com/algorithm/QueueSet.java
@@ -13,6 +13,19 @@ public class QueueSet {
         TreeNode right = null;
     }
 
+    class Node {
+        // 树结点中的元素值
+        int val = 0;
+        // 二叉树结点的左子结点
+        Node left = null;
+        // 二叉树结点的右子结点
+        Node right = null;
+
+        Node next = null;
+    }
+
+
+
     /**
      * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
      * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
@@ -44,5 +57,38 @@ public class QueueSet {
             result.add(cur);
         }
         return result;
+    }
+
+    /**
+     * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+     * https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/submissions/
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        if(root == null){
+            return null;
+        }
+        List<Node> cur = new ArrayList<>();
+        cur.add(root);
+        List<Node> next = null;
+        while (!cur.isEmpty()){
+            next = new ArrayList<>();
+            for(int i =0 ;i<cur.size();i++){
+                Node node = cur.get(i);
+                if(i+1<cur.size()){
+                    node.next = cur.get(i+1);
+                }
+                if(node.left!=null){
+                    next.add(node.left);
+                }
+                if(node.right!=null){
+                    next.add(node.right);
+                }
+            }
+            cur = next;
+            next = null;
+        }
+        return root;
     }
 }
